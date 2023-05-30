306ebabfb4b76d8e11eac116d796a7e27b2e648d
Escudier Quentin <quentin.escudier@epita.fr>
2023-05-30 13:15:41 +0200

    feat: start bash script added

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -d node_modules ] ; then
+  echo "node-modules does not exist"
+  yarn install
+fi
+
+NOTION_DATABASE_ID_LEARNINGS=1ddba9742f774f94862bba26353fa3ec
+NOTION_DATABASE_ID_STUDENTS=1378c199b5e048ff85a78e3140f1f693
+NOTION_DATABASE_ID_COURSES=678d0188294b4d4bb30231ff5a851c2a
+NOTION_DATABASE_ID_PROOFS=ae8d0dda243448ac85e4b9b579e3a4c0
+
+NOTION_KEY=secret_doG5lBclQtRu4pB40UaUnn0YMVUN4Ah9bOrAMiqdtO7
+yarn start
